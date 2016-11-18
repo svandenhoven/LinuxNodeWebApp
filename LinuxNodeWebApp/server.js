94d3f9939d4951b4850f7a60f4ad8d1d8504e1b9
Sander van den Hoven <sandervd@microsoft.com>
2016-11-18 16:43:24 +0100

    test

--- LinuxNodeWebApp/server.js
+++ LinuxNodeWebApp/server.js
@@ -4,5 +4,5 @@ var port = process.env.port || 1337;
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Linux World\n');
+    res.end('Hello Linux World\n'); 
 }).listen(port);
